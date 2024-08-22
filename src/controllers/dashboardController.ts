4a9014d9bf86b3abb9c2cbd6c87d917cf55639bd
Hoooooou-Jun <jipkim2@gmail.com>
2024-08-23 02:07:41 +0900

    test: dashboard API 구조 구현

---
+++ src/controllers/dashboardController.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express';
+import logger from '@utils/logger';
+
+const dashboardService = new DashboardService();
+
+class DashboardController {
+  async getDashboard(req: Request, res: Response) {
+    try {
+      const getDashboard: any = await dashboardService.getDashboard();
+      if (getDashboard.ok) {
+        logger.http(`getAllYoutube`);
+        return res.status(200).json({
+          message: "Success request dashboard",
+          youtube: getDashboard.youtube
+        });
+      }
+      return res.status(401).json({
+        message: getDashboard.message,
+      });
+    } catch (err: any) {
+      logger.error("Dashboard controller error:", err)
+      return res.status(500).json({
+        message: err.message,
+        err: err
+      });
+    }
+  }
+}
+
+export default DashboardController;
